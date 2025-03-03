0a6a9ab821076206bfe5326b629652122bca8dfe
moyudoing <moyudoing@users.noreply.github.com>
2025-03-03 16:05:17 +0000

    deploy: 939a77767ad5ff5116c635f985f2ac03b4d5b4c3

---
+++ assets/select-D91Z32IC.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-CrNqDM1D.js";const a=e("selectStore",{state:()=>({formData:{selectedColor:"",largeBallCount:0,smallBallCount:0,selectedPosition:""}}),actions:{updateFormData(t){this.formData=t}}});export{a as u};
